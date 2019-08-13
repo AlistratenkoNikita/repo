16ebba00fbe24531a0312336c4e6c7fcb3923508
Nikita Alistratenko <steelheroe@gmail.com>
2019-08-13 13:21:24 +0300

    Commit #3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("a2");
+        System.out.println("a3");
     }
 }
 
@@ -9,5 +9,9 @@ class A1{
 }
 
 class A2{
+
+}
+
+class A3{
 
 }
